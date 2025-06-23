3d354e7da5628d9883f7446e895ebfcae5f339ce
junseoko3o <github.yam117@passinbox.com>
2025-06-23 22:05:01 +0900

    Feat: 유저 컬럼 추가

--- src/main/java/com/simple/chat/entity/User.java
+++ src/main/java/com/simple/chat/entity/User.java
@@ -1,6 +1,10 @@
 package com.simple.chat.entity;
 
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class User {
@@ -9,6 +13,23 @@ public class User {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false, unique = true)
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column()
     private String name;
+
+    @Column()
+    private String password;
+
+    @Column(name = "login_at")
+    private LocalDateTime loginAt;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    protected LocalDateTime updatedAt;
 }
